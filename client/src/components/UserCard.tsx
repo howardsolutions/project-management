32e0ca78faebe33f2b7fa2fb44fdea0b88eb2834
thinhphungdev <phungthinh0946519356@gmail.com>
2024-10-16 12:34:04 +0700

    feat: task card, project card, user card components and search page

---
+++ client/src/components/UserCard.tsx
@@ -0,0 +1,30 @@
+import { User } from "@/state/api";
+import Image from "next/image";
+import React from "react";
+
+type Props = {
+  user: User;
+};
+
+const UserCard = ({ user }: Props) => {
+  return (
+    <div className="flex items-center rounded border p-4 shadow">
+      {user.profilePictureUrl && (
+        <Image
+        src={"/p3.jpeg"}
+        //   src={`https://pm-s3-images.s3.us-east-2.amazonaws.com/p1.jpeg`}
+          alt="profile picture"
+          width={32}
+          height={32}
+          className="rounded-full"
+        />
+      )}
+      <div>
+        <h3>{user.username}</h3>
+        <p>{user.email}</p>
+      </div>
+    </div>
+  );
+};
+
+export default UserCard;
--- client/src/components/index.tsx
+++ client/src/components/index.tsx
@@ -4,12 +4,16 @@ import { ThemeProvider } from "./ThemeProvider";
 import TaskCard from "./TaskCard";
 import ModalCreateNewProject from "./ModalCreateNewProject";
 import ModalCreateNewTask from "./ModalCreateNewTask";
+import ProjectCard from "./ProjectCard";
+import UserCard from "./UserCard";
 
 export {
   Navbar,
   Sidebar,
   ThemeProvider,
   TaskCard,
+  ProjectCard,
   ModalCreateNewProject,
   ModalCreateNewTask,
+  UserCard,
 };

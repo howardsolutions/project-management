6fe568b43e239f466bc10ce117ee4c2fcaca3f3b
thinhphungdev <phungthinh0946519356@gmail.com>
2024-10-18 10:57:11 +0700

    chore: setup backend for ec2 connection

--- server/src/index.ts
+++ server/src/index.ts
@@ -49,4 +49,4 @@ app.use("/teams", teamRoutes);
 // SERVER
 const PORT = process.env.PORT || 4000;
 
-app.listen(PORT, () => console.log(`Server is running on ${PORT}`));
+app.listen(+PORT, '0.0.0.0', () => console.log(`Server is running on ${PORT}`));
